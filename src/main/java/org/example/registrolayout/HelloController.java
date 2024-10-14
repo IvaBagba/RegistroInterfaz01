3ba8b7c1e8edead88cb886f667c03a98f9fec577
ivan baguena <baguena.delabandera.ivan@gmail.com>
2024-10-14 23:03:05 +0200

    terminado

--- src/main/java/org/example/registrolayout/HelloController.java
+++ src/main/java/org/example/registrolayout/HelloController.java
@@ -1,15 +1,12 @@
 package org.example.registrolayout;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
-
     @FXML
     private Button sigInicio;
     @FXML
@@ -25,6 +22,26 @@ public class HelloController {
     private TabPane tabPane;
 
     @FXML
+    private TextField txtNombreCompleto;
+    @FXML
+    private TextField txtTelefono;
+    @FXML
+    private TextField txtCorreo;
+    @FXML
+    private CheckBox paisES;
+    @FXML
+    private CheckBox paisIT;
+
+    @FXML
+    private Label txtNombreCompletoValid;
+    @FXML
+    private Label txtTelefonoValid;
+    @FXML
+    private Label txtCorreoValid;
+    @FXML
+    private Label paisCheck;
+
+    @FXML
     protected void onBtnSalirButtonClick() {
         Stage stage = (Stage) btnSalir.getScene().getWindow();
         stage.close();
@@ -37,6 +54,23 @@ public class HelloController {
 
     @FXML
     protected void onSigRegistroButtonClick() {
+        String paisSeleccionado;
+
+        if (paisES.isSelected() && paisIT.isSelected()) {
+            paisSeleccionado = "ESPAÑA-ITALIA";
+        } else if (paisIT.isSelected()) {
+            paisSeleccionado = "ITALIA";
+        } else if (paisES.isSelected()) {
+            paisSeleccionado = "ESPAÑA";
+        } else {
+            paisSeleccionado = "NULL";
+        }
+
+        txtNombreCompletoValid.setText("Nombre Completo: " + txtNombreCompleto.getText());
+        txtTelefonoValid.setText("Telefono: " + txtTelefono.getText());
+        txtCorreoValid.setText("Correo: " + txtCorreo.getText());
+        paisCheck.setText(paisSeleccionado);
+
         tabPane.getSelectionModel().select(2);
     }
 
